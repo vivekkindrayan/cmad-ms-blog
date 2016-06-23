4f17ae4653de6d2f8f4249cf7e224ac186fc4261
Amit Agarwal <ami.aga@gmail.com>
2016-06-23 23:30:05 +0530

    Adding Junit dependency and sample Test case

---
+++ src/test/java/com/mysocial/test/BlogTest.java
@@ -0,0 +1,52 @@
+package com.mysocial.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BlogTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testStartFutureOfVoid() {
+		//fail("Not yet implemented"); // TODO
+		System.out.println("testStartFutureOfVoid");
+	}
+
+	@Test
+	public final void testStopFuture() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testDeploy() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testMain() {
+		//fail("Not yet implemented"); // TODO
+		System.out.println("testMain");
+
+	}
+
+}
