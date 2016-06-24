b42625a19fb135d6ab4f5632b03a4b8a0a8f857c
Amit Agarwal <ami.aga@gmail.com>
2016-06-24 09:02:59 +0530

    Junit test for BlogVerticle

--- src/test/java/com/mysocial/test/BlogTest.java
+++ src/test/java/com/mysocial/test/BlogTest.java
@@ -8,7 +8,14 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.mysocial.verticles.BlogVerticle;
+
+import io.vertx.ext.web.Router;
+
 public class BlogTest {
+	
+	BlogVerticle bv;
+	Router r;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -20,32 +27,37 @@ public class BlogTest {
 
 	@Before
 	public void setUp() throws Exception {
+		bv = new BlogVerticle();
+		r = Router.router(null);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		bv = null;
 	}
 
 	@Test
 	public final void testStartFutureOfVoid() {
-		//fail("Not yet implemented"); // TODO
-		System.out.println("testStartFutureOfVoid");
+		System.out.println("Into -> testStartFutureOfVoid");
 	}
 
 	@Test
 	public final void testStopFuture() {
-		//fail("Not yet implemented"); // TODO
+		System.out.println("Into -> testStopFuture");
 	}
 
 	@Test
 	public final void testDeploy() {
-		//fail("Not yet implemented"); // TODO
+		System.out.println("Into -> testDeploy");
+		assertNotNull(bv);
+		assertNotNull(r);
 	}
 
 	@Test
 	public final void testMain() {
-		//fail("Not yet implemented"); // TODO
-		System.out.println("testMain");
+		System.out.println("Into -> testMain");
+		assertNotNull(bv);
+		
 	}
 
 }
